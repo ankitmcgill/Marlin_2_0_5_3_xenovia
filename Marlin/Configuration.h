352e8066bf1353537344baf05eba35948903ddf3
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2020-05-31 23:54:36 +0530

    modified Y_BED_SIZE to 250

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1093,7 +1093,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 0
-#define Y_BED_SIZE 280
+#define Y_BED_SIZE 250
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
